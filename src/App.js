1dcd241db225c1624f819717152946da1d1c5811
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2022-04-11 05:00:10 -0400

    changed App from function to class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import {Component} from "react";
 import Profile from './components/Profile'
 import Clock from './components/Clock'
 
-function App() {
-  return (
-    <div className="App">
-      <Profile/>
-      <Clock/>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+      return (
+          <div className="App">
+              <Profile/>
+              <Clock/>
+          </div>
+    );
+  }
 }
-
 export default App;
